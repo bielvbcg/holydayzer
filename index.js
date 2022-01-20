e44e2ad8b6637e1609632870efbe6cc6bd1e368f
bielvbcg <gabrielvbcallegario@gmail.com>
2022-01-20 13:17:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const server = express()
+const hoje = new Date();
+const mes = hoje.toLocaleDateString().split("/")[0];
+
+const holidays = [
+  { date: "1/1/2022", name: "Confraternização mundial" },
+  { date: "1/3/2022", name: "Carnaval" },
+  { date: "4/17/2022", name: "Páscoa" },
+  { date: "4/21/2022", name: "Tiradentes" },
+  { date: "5/1/2022", name: "Dia do trabalho" },
+  { date: "6/16/2022", name: "Corpus Christi" },
+  { date: "9/7/2022", name: "Independência do Brasil" },
+  { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+  { date: "11/2/2022", name: "Finados" },
+  { date: "11/15/2022", name: "Proclamação da República" },
+  { date: "12/25/2022", name: "Natal" }
+];
+
+server.get("/holidays", (req, res) => {
+  res.send(holidays)
+})
+
+server.get("/is-today-holiday", (req, res) => {
+  let isTodayHoliday = "Não, hoje não é feriado"
+
+  holidays.map(holiday => holiday.date === hoje.toLocaleDateString() ? isTodayHoliday = `Sim, hoje é ${holiday.name}` : "")
+
+  res.send(isTodayHoliday)
+})
+
+server.get(`/holidays/:mes`, (req, res) => {
+  const mes = parseInt(req.params.mes)
+
+  resposta = holidays.filter(holiday => {
+    const mesFeriado = parseInt(holiday.date.split("/")[0])
+
+    if (mes === mesFeriado) return true
+  })
+
+  res.send(resposta)
+})
+
+server.listen(3000)
